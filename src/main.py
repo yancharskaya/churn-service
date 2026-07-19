1525344e948fe48ac0ba7e01732b39760036c253
yancharskaya <y.yancharskaya@gmail.com>
2026-07-19 20:00:21 +0300

    refactor: allow to run server and train model without mlflow

--- src/main.py
+++ src/main.py
@@ -1,28 +1,40 @@
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
-import logging, mlflow.sklearn, os
+import logging
+import mlflow.sklearn
+import os
+import joblib
 
 logging.basicConfig(
-    level=logging.INFO,   # показывать INFO и важнее
-    format='%(asctime)s %(levelname)s %(name)s: %(message)s')
+    level=logging.INFO,  # показывать INFO и важнее
+    format="%(asctime)s %(levelname)s %(name)s: %(message)s",
+)
 log = logging.getLogger(__name__)
 
 app = FastAPI()
 
-mlflow.set_tracking_uri(os.getenv('MLFLOW_TRACKING_URI',
-                                  'http://localhost:5000'))
-model = mlflow.sklearn.load_model('models:/churn-model@champion')
+mlflow_uri = os.getenv("MLFLOW_TRACKING_URI")
+if mlflow_uri:
+    mlflow.set_tracking_uri(mlflow_uri)
+    model = mlflow.sklearn.load_model("models:/churn-model@champion")
+else:
+    model = joblib.load("model.pkl")
 
-log.info('model loaded')
+log.info("model loaded")
 
-class Features(BaseModel):                                                        # breast_cancer имеет 30 признаков;
-    values: list[float] = Field(..., min_length=30, max_length=30)                # для простоты принимаем списком
 
-@app.get('/health')
+class Features(BaseModel):  # breast_cancer имеет 30 признаков;
+    values: list[float] = Field(
+        ..., min_length=30, max_length=30
+    )  # для простоты принимаем списком
+
+
+@app.get("/health")
 def health():
-    return {'status': 'ok'}
+    return {"status": "ok"}
 
-@app.post('/predict')
+
+@app.post("/predict")
 def predict(f: Features):
     pred = model.predict([f.values])
-    return {'prediction': int(pred[0])}
+    return {"prediction": int(pred[0])}
--- train/train.py
+++ train/train.py
@@ -1,4 +1,5 @@
-import mlflow, mlflow.sklearn
+import mlflow
+import mlflow.sklearn
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
@@ -7,39 +8,64 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import f1_score
 import os
 import pandas as pd
+import joblib
 import logging
+
 logging.basicConfig(
-    level=logging.INFO,   # показывать INFO и важнее
-    format='%(asctime)s %(levelname)s %(name)s: %(message)s')
+    level=logging.INFO,  # показывать INFO и важнее
+    format="%(asctime)s %(levelname)s %(name)s: %(message)s",
+)
 log = logging.getLogger(__name__)
 
-host = os.getenv("HOST", 'http://localhost:5000')
-churn = os.getenv("CHURN", 'churn')
+mlflow_uri = os.getenv("MLFLOW_TRACKING_URI")
 
-mlflow.set_tracking_uri(host)                      # сервер из compose
-mlflow.set_experiment(churn)                       # папка для run'ов
-
-data_path = os.getenv("DATA")                      # DATA="data/train.csv"
+data_path = os.getenv("DATA_PATH")
 if data_path:
     df = pd.read_csv(data_path)
-    X, y = df.drop('target', axis=1), df['target']
+    X, y = df.drop("target", axis=1), df["target"]
 else:
     X, y = load_breast_cancer(return_X_y=True)
 
-X_tr, X_te, y_tr, y_te = train_test_split(X, y, test_size=0.25,
-                                          stratify=y, random_state=42)
+X_tr, X_te, y_tr, y_te = train_test_split(
+    X, y, test_size=0.25, stratify=y, random_state=42
+)
 
-C = float(os.getenv("C") or 1.0)                  # меняйте между запусками
-with mlflow.start_run():                          # начало «записи»
-    mlflow.log_param('model', 'logreg')           # записать параметр
-    mlflow.log_param('C', C)
-    pipe = Pipeline([('sc', StandardScaler()),
-                     ('lr', LogisticRegression(C=C, max_iter=1000))])
+
+def train_model_mlflow():
+    churn = os.getenv("CHURN", "churn")
+    C = float(os.getenv("C") or 1.0)
+    mlflow.set_tracking_uri(mlflow_uri)  # сервер из compose
+    mlflow.set_experiment(churn)  # папка для run'ов
+    with mlflow.start_run():  # начало «записи»
+        mlflow.log_param("model", "logreg")  # записать параметр
+        mlflow.log_param("C", C)
+        pipe = Pipeline(
+            [("sc", StandardScaler()), ("lr", LogisticRegression(C=C, max_iter=1000))]
+        )
+        pipe.fit(X_tr, y_tr)
+        f1 = f1_score(y_te, pipe.predict(X_te))
+        mlflow.log_metric("f1", f1)  # записать метрику
+        mlflow.sklearn.log_model(  # записать саму модель
+            pipe,
+            name="model",  #   (артефакт → в MinIO!)
+            registered_model_name="churn-model",
+        )  #   и в реестр
+        log.info(
+            f"Model trained successfully on dataset with {len(X)} rows using C={C}"
+        )
+        log.info(f"f1:, {f1}")
+
+
+def train_model_local():
+    pipe = Pipeline(
+        [("sc", StandardScaler()), ("lr", LogisticRegression(max_iter=1000))]
+    )
     pipe.fit(X_tr, y_tr)
-    f1 = f1_score(y_te, pipe.predict(X_te))
-    mlflow.log_metric('f1', f1)                   # записать метрику
-    mlflow.sklearn.log_model(                     # записать саму модель
-        pipe, name='model',                       #   (артефакт → в MinIO!)
-        registered_model_name='churn-model')      #   и в реестр
-    log.info(f'Model trained successfully on dataset with {len(X)} rows using C={C}')
-    log.info(f'f1:, {f1}')
+    log.info(f"f1:, {f1_score(y_te, pipe.predict(X_te))}")
+    joblib.dump(pipe, "model.pkl")
+
+
+if mlflow_uri:
+    train_model_mlflow()
+else:
+    train_model_local()
